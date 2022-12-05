e8fb1c206a06215c37a463eceee8121adfbeb042
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2022-12-05 09:46:25 +0100

    day 5 part 1

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -4,9 +4,9 @@ use std::{path::Path, str::FromStr};
 #[derive(Debug, Clone, Copy, parse_display::FromStr, Display)]
 #[display("move {qty} from {origin} to {destination}")]
 struct Movement {
-    qty: u32,
-    origin: u32,
-    destination: u32,
+    qty: usize,
+    origin: usize,
+    destination: usize,
 }
 
 #[derive(Debug, Clone)]
@@ -36,16 +36,31 @@ impl FromStr for Stacks {
         let mut stacks = vec![Vec::new(); n_stacks];
 
         for line in lines {
-            for (idx, chunk) in line.split_whitespace().enumerate() {
-                let chunk = chunk.as_bytes();
-                if chunk[0] != b'[' || chunk[2] != b']' {
+            let line = line.as_bytes();
+            for (idx, stack) in stacks.iter_mut().enumerate() {
+                let start = idx * 4;
+                if line.len() < start + 3 {
+                    break;
+                }
+                if line[start] == b' ' && line[start + 2] == b' ' {
+                    continue;
+                } else if line[start] != b'[' || line[start + 2] != b']' {
                     return Err(Error::MalformedStacks("did not find crate edges"));
                 }
-                stacks[idx].push(chunk[1]);
+                stack.push(line[start + 1]);
             }
         }
 
         Ok(Stacks(stacks))
+    }
+}
+
+impl Stacks {
+    fn tops(&self) -> String {
+        self.0
+            .iter()
+            .map(|stack| stack.last().copied().unwrap_or(b' ') as char)
+            .collect()
     }
 }
 
@@ -73,9 +88,21 @@ fn parse(input: &Path) -> Result<(Stacks, Vec<Movement>), Error> {
 }
 
 pub fn part1(input: &Path) -> Result<(), Error> {
-    let (stacks, movements) = parse(input)?;
-    dbg!(stacks.0.len(), movements.len());
-    unimplemented!("input file: {:?}", input)
+    let (mut stacks, movements) = parse(input)?;
+
+    for (movement_idx, movement) in movements.iter().enumerate() {
+        for _ in 0..movement.qty {
+            let origin = movement.origin - 1; // movements are 1-indexed
+            let destination = movement.destination - 1; // movements are 1-indexed
+            let top = stacks.0[origin]
+                .pop()
+                .ok_or(Error::StackUnderflow(movement_idx, origin))?;
+            stacks.0[destination].push(top);
+        }
+    }
+
+    println!("stack tops: {}", stacks.tops());
+    Ok(())
 }
 
 pub fn part2(input: &Path) -> Result<(), Error> {
@@ -92,4 +119,6 @@ pub enum Error {
     MalformedStacks(&'static str),
     #[error(transparent)]
     TwoPhase(#[from] aoclib::input::TwoPhaseError),
+    #[error("stack underflow in movement {0} stack {1}")]
+    StackUnderflow(usize, usize),
 }
