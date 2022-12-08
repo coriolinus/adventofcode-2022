c409f8da1d9f319ea6ed6997d941832e8feb9e33
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2022-12-08 09:23:24 +0100

    day 8 part 2
    Somehow I'd imagined that part 2 would require something more clever
    than just naively computing the scenic score for every point on the map,
    but this worked out just fine. Hooray for fast hardware!

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -28,6 +28,34 @@ fn filter_visible<'a>(
     })
 }
 
+/// Return those coordinates which are on the map and visible from the specified projection.
+fn visible_from(
+    map: &Map<Digit>,
+    origin: Point,
+    direction: Direction,
+) -> impl '_ + Iterator<Item = Point> {
+    let mut found_blocker = false;
+    let (dx, dy) = direction.deltas();
+    let initial_height = map[origin];
+    map.project(origin, dx, dy)
+        // origin is the first point in the projection
+        .skip(1)
+        .take_while(move |point| {
+            if found_blocker {
+                false
+            } else {
+                found_blocker |= map[*point] >= initial_height;
+                true
+            }
+        })
+}
+
+fn scenic_score(map: &Map<Digit>, origin: Point) -> usize {
+    Direction::iter()
+        .map(|direction| visible_from(map, origin, direction).count())
+        .product()
+}
+
 pub fn part1(input: &Path) -> Result<(), Error> {
     let trees = <Map<Digit> as TryFrom<&Path>>::try_from(input)?;
     let n_visible = Direction::iter()
@@ -43,7 +71,21 @@ pub fn part1(input: &Path) -> Result<(), Error> {
 }
 
 pub fn part2(input: &Path) -> Result<(), Error> {
-    unimplemented!("input file: {:?}", input)
+    // We can't re-use the result from part 1 to filter the points to consider here.
+    // Consider a map whose perimeter trees all have height 9. They all have a scenic
+    // score of 0, because there is at least one direction in which they can see no
+    // other trees at all. However, they block all potential inner trees which might have a
+    // higher score.
+
+    let trees = <Map<Digit> as TryFrom<&Path>>::try_from(input)?;
+    let Some(max_scenic_score) = trees.points().map(|point| scenic_score(&trees, point)).max() else {
+        println!("map has size 0");
+        return Ok(())
+    };
+
+    println!("max scenic score: {max_scenic_score}");
+
+    Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
