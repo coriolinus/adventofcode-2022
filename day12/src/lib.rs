12af59072dd6083453a35ba6fbaeece56bb94cfc
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2022-12-12 10:14:31 +0100

    day 12 part 2

--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -51,6 +51,47 @@ fn find_path_to_destination(map: &HeightMap) -> Option<Rc<PathNode>> {
     None
 }
 
+fn find_shortest_hiking_path(map: &HeightMap) -> Option<Rc<PathNode>> {
+    let mut visited = HashSet::new();
+    let mut queue = VecDeque::new();
+    queue.push_back(Rc::new(PathNode {
+        location: map.target,
+        prev: None,
+    }));
+
+    while let Some(node) = queue.pop_front() {
+        // first instance of a location at height 0 gets returned
+        if map[node.location] == 0 {
+            return Some(node);
+        }
+
+        if !visited.insert(node.location) {
+            // we've already visited this location
+            continue;
+        }
+
+        queue.extend(
+            map.adjacencies(node.location)
+                .filter(|&location| {
+                    // we can't travel diagonally
+                    (location - node.location).manhattan() == 1
+                        // we can't descend more than one step at a time
+                        && map[location] >= map[node.location] - 1
+                        // we don't want to go backwards
+                        && !visited.contains(&location)
+                })
+                .map(|location| {
+                    Rc::new(PathNode {
+                        location,
+                        prev: Some(node.clone()),
+                    })
+                }),
+        )
+    }
+
+    None
+}
+
 // wrong, too low: 374
 pub fn part1(input: &Path) -> Result<(), Error> {
     let map = HeightMap::new(input)?;
@@ -64,7 +105,14 @@ pub fn part1(input: &Path) -> Result<(), Error> {
 }
 
 pub fn part2(input: &Path) -> Result<(), Error> {
-    unimplemented!("input file: {:?}", input)
+    let map = HeightMap::new(input)?;
+    let path_to_destination = find_shortest_hiking_path(&map).ok_or(Error::NoSolution)?;
+    let node_count = path_to_destination.iter().count();
+    // we can't parse a map on which the start and target positions are identical, so we know that there
+    // are at least two nodes in every correct solution.
+    let step_count = node_count - 1;
+    println!("steps in shortest possible path: {step_count}");
+    Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
