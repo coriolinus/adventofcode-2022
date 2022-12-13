bbd37352a1532a12ee6f17ea0dd2171cb2ddb301
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2022-12-13 11:40:56 +0100

    day 13 part 2

--- day13/src/lib.rs
+++ day13/src/lib.rs
@@ -1,25 +1,70 @@
 use aoclib::input::parse_newline_sep;
 use derive_more::From;
-use std::{path::Path, str::FromStr};
+use std::{cmp::Ordering, path::Path, str::FromStr};
 
 mod parse;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct Integer(u32);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct List(Vec<Value>);
 
-impl List {
-    fn iter(&self) -> impl '_ + Iterator<Item = &Value> {
-        self.0.iter()
+impl PartialOrd for List {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
-#[derive(Debug, Clone, From)]
+impl Ord for List {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let mut left = self.0.iter();
+        let mut right = other.0.iter();
+
+        loop {
+            return match (left.next(), right.next()) {
+                (Some(left), Some(right)) if left == right => {
+                    // in the event the two sides are equal, keep checking
+                    // further items in the list
+                    continue;
+                }
+                (Some(left), Some(right)) => left.cmp(right),
+                (None, Some(_)) => Ordering::Less,
+                (Some(_), None) => Ordering::Greater,
+                (None, None) => Ordering::Equal,
+            };
+        }
+    }
+}
+
+#[derive(Debug, Clone, From, PartialEq, Eq)]
 enum Value {
     List(List),
     Int(Integer),
+}
+
+impl PartialOrd for Value {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[inline]
+fn list_of(int: &Integer) -> Value {
+    List(vec![Value::Int(*int)]).into()
+}
+
+impl Ord for Value {
+    fn cmp(&self, other: &Self) -> Ordering {
+        use Value::{Int, List};
+
+        match (self, other) {
+            (Int(left), Int(right)) => left.cmp(right),
+            (List(left), List(right)) => left.cmp(right),
+            (List(_), Int(right)) => self.cmp(&list_of(right)),
+            (Int(left), List(_)) => list_of(left).cmp(other),
+        }
+    }
 }
 
 struct Pair {
@@ -45,52 +90,11 @@ impl FromStr for Pair {
     }
 }
 
-#[inline]
-fn list_of(int: &Integer) -> List {
-    List(vec![Value::Int(*int)])
-}
-
-fn is_ordered(left: &Value, right: &Value) -> Option<bool> {
-    use std::cmp::Ordering::{Equal, Greater, Less};
-    use Value::{Int, List};
-
-    match (left, right) {
-        (Int(left), Int(right)) => match left.cmp(right) {
-            Less => Some(true),
-            Equal => None,
-            Greater => Some(false),
-        },
-        (List(left), List(right)) => compare_lists(left, right),
-        (List(_), Int(right)) => is_ordered(left, &list_of(right).into()),
-        (Int(left), List(_)) => is_ordered(&list_of(left).into(), right),
-    }
-}
-
-fn compare_lists(left: &List, right: &List) -> Option<bool> {
-    let mut left = left.iter();
-    let mut right = right.iter();
-
-    loop {
-        match (left.next(), right.next()) {
-            (Some(left), Some(right)) => {
-                let potential_order = is_ordered(left, right);
-                if potential_order.is_some() {
-                    return potential_order;
-                }
-            }
-            (None, Some(_)) => return Some(true),
-            (Some(_), None) => return Some(false),
-            (None, None) => return None,
-        }
-    }
-}
-
 pub fn part1(input: &Path) -> Result<(), Error> {
     let ordered_pairs_sum: u32 = parse_newline_sep::<Pair>(input)?
         .enumerate()
         .filter_map(|(idx, Pair { left, right })| {
-            compare_lists(&left, &right)
-                .unwrap_or_default()
+            (left < right)
                 // AoC indices start from 1
                 .then_some(idx as u32 + 1)
         })
@@ -100,7 +104,37 @@ pub fn part1(input: &Path) -> Result<(), Error> {
 }
 
 pub fn part2(input: &Path) -> Result<(), Error> {
-    unimplemented!("input file: {:?}", input)
+    let divider_two = List::from_str("[[2]]").unwrap();
+    let divider_six = List::from_str("[[6]]").unwrap();
+
+    let input = std::fs::read_to_string(input)?;
+    let mut packets = input
+        .lines()
+        .map(str::trim)
+        .filter(|line| !line.is_empty())
+        .map(List::from_str)
+        .collect::<Result<Vec<_>, _>>()?;
+    packets.push(divider_two.clone());
+    packets.push(divider_six.clone());
+    packets.sort();
+
+    let two_idx = packets
+        .iter()
+        .position(|item| *item == divider_two)
+        .expect("we have definitely included this divider packet")
+        // AoC indices are 1-based
+        + 1;
+    let six_idx = packets
+        .iter()
+        .position(|item| *item == divider_six)
+        .expect("we have definitely included this divider packet")
+        // AoC indices are 1-based
+        + 1;
+
+    let decoder_key = two_idx * six_idx;
+    println!("decoder key: {decoder_key}");
+
+    Ok(())
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -109,4 +143,6 @@ pub enum Error {
     Io(#[from] std::io::Error),
     #[error("no solution found")]
     NoSolution,
+    #[error("parsing input")]
+    Parse(#[from] parse::ParseError),
 }
